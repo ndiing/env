ded9c67e0625da8b817acaf3b4f0e35ef7645048
Ridho Prasetya <ndiing.inc@gmail.com>
2024-10-18 17:42:14 +0700

    commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const fs = require("fs");
  * @throws {Error} - Melempar error jika terjadi kesalahan saat membaca file.
  */
 function read(filename = ".env") {
-    const env = fs.readFileSync(filename, { encoding: "utf8" });
+    const env = fs.readFileSync(path.join(process.cwd(),filename), { encoding: "utf8" });
 
     for (const [, name, value1, value2, value3, value4] of env.matchAll(/([A-Z_]+)\s*=\s*(?:"([\s\S]+?)"|'([\s\S]+?)'|`([\s\S]+?)`|([\s\S]+?))\s*(?:#|\n|$)/g)) {
         const value = value4 || value3 || value2 || value1;
